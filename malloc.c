5ae7f3856c0453025fe57022ef889020ca491a13
KsushaVald <ksushavald@yandex.ru>
2017-06-23 15:18:03 +0600

    ++

--- malloc.c
+++ malloc.c
@@ -2,25 +2,59 @@
 #include <malloc.h>
 
 struct spisok{
-  void* adress;
-  int  size;
+  void* address;
+  size_t  size;
   int  state;
   struct spisok* next;
   struct spisok* previous;
 };
-
+struct spisok *memory=NULL;
 
 void* new_malloc(size_t size, const void*a)
 {
-  void *p;
-  p=sbrk(0);
-  sbrk(size);
-  return p;
+  if(memory==NULL){
+    memory->address=sbrk(0);
+    memory->size=size;
+    memory->state=1;
+    memory->previous=NULL;
+    memory->next=NULL;
+    sbrk(size);
+    return memory->address;
+  }
+  else{
+    while( (memory->next!=NULL) || ((memory->next->state!=0)&&(memory->next->size>=size)) ){
+        memory=memory->next;
+    }
+    if(memory->next==NULL){
+        memory->next->previous=memory;
+        memory=memory->next;
+        memory->address=sbrk(0);
+        memory->size=size;
+        memory->state=1;
+        memory->next=NULL;
+        sbrk(size);
+        return memory->address;
+    }
+    else{
+        memory=memory->next;
+        memory->state=1;
+        return memory->address;
+    }
+  }
 }
 
 void new_free(void*name, const void*b)
 {
-    brk(name);
+    while(memory->address!=name){
+        memory=memory->next;
+    }
+    if(memory->next==NULL){
+        memory->previous->next=NULL;
+        brk(name);
+    }
+    else{
+        memory->state=0;
+    }
 }
 
 void init(void)
@@ -32,18 +66,18 @@ void (* __malloc_initialize_hook)(void)=init;
 
 int main()
 {
-    int *a, *b, *c;
+    int *a, *b, *c, *e;
     a=malloc(sizeof(int));
     a=10;
     b=malloc(sizeof(int));
     b=11;
     c=malloc(sizeof(int));
     c=12;
-    printf("%d %d %d\n", a, b, c);
-    printf("%d %d %d\n", &a, &b, &c);
+    printf("%d %d %d\n", *a, *b, *c);
+    printf("%p %p %p\n", a, b, c);
     free(b);
-    b=15;
-    printf("%d %d %d\n", a, b, c);
-    printf("%d %d  %d\n", &a,&b, &c);
+    e=malloc(sizeof(int));
+    printf("%d %d %d\n", *a, *c, *e);
+    printf("%p %p  %p\n",a, c, e);
 
 }
